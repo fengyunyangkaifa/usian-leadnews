79a89b7c66b37d3aea01d2b09c2231654ba85da0
fengyunyang <2995299866qq.com>
2022-08-15 09:00:45 +0800

    canal技术 8.13

--- usian-leadnews-user/src/main/java/com/usian/user/config/RabbitMQConfig.java
+++ usian-leadnews-user/src/main/java/com/usian/user/config/RabbitMQConfig.java
@@ -1,6 +1,6 @@
 package com.usian.user.config;
+
 import org.springframework.amqp.core.*;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,25 +8,62 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String EXCHANGE_MESSAGE="exchange_message";
-    public static final String QUEUE_MESSAGE="queue_message";
+//    public static final String EXCHANGE_MESSAGE="exchange_message";
+//    public static final String QUEUE_MESSAGE="queue_message";
 
     //    directExchange  routingKey模式  fanout 发布订阅者模式 topic 主题模式   header  头信息模式
 //    创建交换机
-    @Bean(EXCHANGE_MESSAGE)
-    public Exchange EXCHANGE_MESSAGE(){
-        //  durable  交换机是否持久化
-        return ExchangeBuilder.directExchange(EXCHANGE_MESSAGE).durable(true).build();
-    }
-//    创建队列
-    @Bean(QUEUE_MESSAGE)
-    public Queue QUEUE_MESSAGE(){
-        return new Queue(QUEUE_MESSAGE);
-    }
-//    关联交换机，队列
-    @Bean
-    public Binding EXCHANGE_MESSAGEandQUEUE_MESSAGE(@Qualifier(EXCHANGE_MESSAGE) Exchange exchange, @Qualifier(QUEUE_MESSAGE) Queue queue){
-        return BindingBuilder.bind(queue).to(exchange).with("auth").noargs();
-    }
+//    @Bean(EXCHANGE_MESSAGE)
+//    public Exchange EXCHANGE_MESSAGE(){
+//        //  durable  交换机是否持久化
+//        return ExchangeBuilder.directExchange(EXCHANGE_MESSAGE).durable(true).build();
+//    }
+////    创建队列
+//    @Bean(QUEUE_MESSAGE)
+//    public Queue QUEUE_MESSAGE(){
+//        return new Queue(QUEUE_MESSAGE);
+//    }
+////    关联交换机，队列
+//    @Bean
+//    public Binding EXCHANGE_MESSAGEandQUEUE_MESSAGE(@Qualifier(EXCHANGE_MESSAGE) Exchange exchange, @Qualifier(QUEUE_MESSAGE) Queue queue){
+//        return BindingBuilder.bind(queue).to(exchange).with("auth").noargs();
+//    }
 
+  /**
+  * 延迟队列
+  */
+//  交换机
+  public static final String TTL_EXCHANGE="ttl_exchange";
+//   死信队列
+  public static final String TTL_QUEUE="ttl_queue";
+//   接替队列
+  public static final String TTL_TEST="ttl_test";
+
+
+  //  创建交换机
+    @Bean
+    public DirectExchange TTL_EXCHANGE(){
+        return new DirectExchange(TTL_EXCHANGE);
+    }
+//    持久化队列
+      @Bean
+    public Queue TTL_TEST(){    //  接替队列持久化
+        //  durable  交换机是否持久化
+        return new Queue(TTL_TEST,true);
+    }
+//   消息发送到死信队列
+    @Bean
+    public Queue TTL_QUEUE(){
+        return QueueBuilder.durable(TTL_QUEUE)  //  死信队列
+                .withArgument("x-dead-letter-exchange",TTL_EXCHANGE)    // 对应的交换机    x-dead-letter-exchange key 固定
+                .withArgument("x-dead-letter-routing-key",TTL_TEST)   //  接替的队列   x-dead-letter-routing-key key固定  routing_key
+                .build();
+    }
+    //  进行绑定
+    @Bean     //  先绑定普通队列   //  再绑定交换机
+    public Binding BangDing(Queue TTL_TEST, DirectExchange TTL_EXCHANGE){
+          return BindingBuilder.bind(TTL_TEST)
+                  .to(TTL_EXCHANGE)
+                  .with("ttl_test");
+    }
 }
--- usian-leadnews-user/src/main/java/com/usian/user/rabbitMQListener/Listener.java
+++ usian-leadnews-user/src/main/java/com/usian/user/rabbitMQListener/Listener.java
@@ -3,20 +3,33 @@ package com.usian.user.rabbitMQListener;
 import com.usian.model.user.dtos.AuthDto;
 import com.usian.user.config.RabbitMQConfig;
 import com.usian.user.service.ApUserRealnameService;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
 public class Listener {
-    @Autowired
-    private ApUserRealnameService apUserRealnameService;
+//    @Autowired
+//    private ApUserRealnameService apUserRealnameService;
 
 //     监听新增信息
-    @RabbitListener(queues = {RabbitMQConfig.QUEUE_MESSAGE})
-    public void authMQ(String msg){
-        AuthDto authDto = new AuthDto();
-        authDto.setId(Integer.valueOf(msg));
-        apUserRealnameService.AutoUpdateStatus(authDto);
+//    @RabbitListener(queues = {RabbitMQConfig.QUEUE_MESSAGE})
+//    public void authMQ(String msg){
+//        AuthDto authDto = new AuthDto();
+//        authDto.setId(Integer.valueOf(msg));
+//        apUserRealnameService.AutoUpdateStatus(authDto);
+//    }
+//    监听私信消息
+    @RabbitListener(queues = {RabbitMQConfig.TTL_TEST})
+    @RabbitHandler
+    public void msg(@Payload Object msg){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("当前时间："+simpleDateFormat.format(new Date()));
+        System.out.println("收到时间："+msg);
     }
 }
