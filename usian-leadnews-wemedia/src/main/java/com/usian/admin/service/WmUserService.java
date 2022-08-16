8c76846fc0c1ca008675deda9fc90d34d2ba8af8
fengyunyang <2995299866qq.com>
2022-08-16 15:47:23 +0800

    修改后

---
+++ usian-leadnews-wemedia/src/main/java/com/usian/admin/service/WmUserService.java
@@ -0,0 +1,8 @@
+package com.usian.admin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.usian.model.media.pojos.WmUser;
+
+public interface WmUserService extends IService<WmUser> {
+
+}
